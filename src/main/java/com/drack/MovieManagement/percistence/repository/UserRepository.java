8ee257aef4a1f2689285578a1070c4e937ad8342
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-09 11:24:01 -0600

    Agregando la etiqueta @Transactional pra que nos funcione el metodo de borrar

--- src/main/java/com/drack/MovieManagement/percistence/repository/UserRepository.java
+++ src/main/java/com/drack/MovieManagement/percistence/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.drack.MovieManagement.percistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByNameContaining(String name);
     Optional<User> findByUsername(String username);
 
+    @Transactional
     @Modifying
     int deleteByUsername(String username);
 }
