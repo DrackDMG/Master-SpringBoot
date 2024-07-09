a6894ac3d1f74d83a20312d50976234213719a06
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-09 11:12:35 -0600

    Creacion de los endpoints de user

--- src/main/java/com/drack/MovieManagement/controller/UserController.java
+++ src/main/java/com/drack/MovieManagement/controller/UserController.java
@@ -4,11 +4,13 @@ import com.drack.MovieManagement.exception.ObjectNotFoundException;
 import com.drack.MovieManagement.percistence.entity.Movie;
 import com.drack.MovieManagement.percistence.entity.User;
 import com.drack.MovieManagement.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,6 +35,33 @@ public class UserController {
     public ResponseEntity<User> findByUsername(@PathVariable String username) {
         try {
             return ResponseEntity.ok(userService.findOneByUsername(username));
+        }catch (ObjectNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user, HttpServletRequest request) {
+        String baseUrl = request.getRequestURL().toString();
+        User createdUser = userService.createOne(user);
+        URI uri = URI.create(baseUrl + "/" + createdUser.getUsername());
+        return ResponseEntity.created(uri).body(createdUser);
+    }
+
+    @PutMapping("/{username}")
+    public ResponseEntity<User> update(@PathVariable String username, @RequestBody User user) {
+        try {
+            return ResponseEntity.ok(userService.updateOneByUsername(username, user));
+        }catch (ObjectNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<Void> delete(@PathVariable String username) {
+        try {
+            userService.deleteOneByUsername(username);
+            return ResponseEntity.noContent().build();
         }catch (ObjectNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
